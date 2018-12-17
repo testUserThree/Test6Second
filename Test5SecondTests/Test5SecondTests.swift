6f3e8d92be7bd5d3d72c69d148a7e2fa3e83345d
Wu <wu@wudemacbook-air.local>
2018-12-17 15:20:10 +0800

    Initial Commit

---
+++ Test5SecondTests/Test5SecondTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test5SecondTests.swift
+//  Test5SecondTests
+//
+//  Created by Wu on 2018/12/17.
+//  Copyright © 2018年 SICNU. All rights reserved.
+//
+
+import XCTest
+@testable import Test5Second
+
+class Test5SecondTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
